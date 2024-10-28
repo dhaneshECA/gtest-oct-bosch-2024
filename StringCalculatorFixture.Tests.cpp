974812b8012fd6daeb744ffaa5127cdedbc315a6
dhaneshECA <dhaneshutd@gmail.com>
2024-10-28 16:49:25 +0530

    Update StringCalculatorFixture.Tests.cpp

--- StringCalculatorFixture.Tests.cpp
+++ StringCalculatorFixture.Tests.cpp
@@ -1,1 +1,20 @@
-#include 
+#include "StringCalculator.h"
+#include <gtest/gtest.h>
+
+class StringCalculatorAddFixture:pblic testing::Test {
+  protected:
+    StringCalculator *objUnderTest;
+    void SetUp() override {
+      objUnderTest = new StringCalculator();
+    }
+    void TearDown() override {
+      delete objUnderTest;
+    }
+};
+
+TEST_F(StringCalculatorAddFixture, Returns_0_for_zero) {
+  int expectedValue = 0;
+  string input = "0";
+  int actualValue = objUnderTest->Add(input);
+  EXPECT_EQ(expectedValue, actualValue);
+}
