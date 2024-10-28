1fe824f9a3678cc51700a6a92c34f1b7c16b451c
dhaneshECA <dhaneshutd@gmail.com>
2024-10-28 15:16:50 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -1,4 +1,17 @@
 #include "StringCalculator.h"
 #include <gtest/gtest.h>
 
+// Tests factorial of 0.
+TEST(when_passed_a_single_number, Returns_0_for_zero) {
+  // Arrange
+  StringCalculator objUnderTest;
+  int expectedValue = 0;
+
+  //Act
+  int actualValue = objUnderTest.Add("0");
+  
+  //Assert
+  EXPECT_EQ(expectedValue, actualValue);
+}
+
 
