8b31027d8b0a855ffcf57a46839cfb919e45d747
dhaneshECA <dhaneshutd@gmail.com>
2024-10-29 11:09:24 +0530

    Update StringCalculatorValueParameter.Tests.cpp

--- StringCalculatorValueParameter.Tests.cpp
+++ StringCalculatorValueParameter.Tests.cpp
@@ -21,7 +21,7 @@ class StringCalculatorAddValueParamFixture: public StringCalculatorAddFixture, p
 
 INSTANTIATE_TEST_SUITE_P(ValidValuesDataSet,StringCalculatorAddValueParamFixture,testing::Values(
 make_tuple("",0), make_tuple("0",0), make_tuple("1",1), make_tuple("1,2,3",6),
-make_tuple("//;\n1;2", 3), make_tuple("//[***]\n8***2***3", 3), make_tuple("//[*][%]\n4*2%3",9), make_tuple("42,1001,3",45)
+make_tuple("//;\n1;2", 3), make_tuple("//[***]\n8***2***3", 13), make_tuple("//[*][%]\n4*2%3",9), make_tuple("42,1001,3",45)
 ));
 
 TEST_P(StringCalculatorAddValueParamFixture, DataDrivenTest) {
@@ -29,4 +29,9 @@ TEST_P(StringCalculatorAddValueParamFixture, DataDrivenTest) {
   int expectedValue = std::get<1>(GetParam());
   int actualValue = objUnderTest->Add(input);
   EXPECT_EQ(expectedValue, actualValue);
+}
+
+TEST_F(StringCalculatorAddFixture, Throws_an_exception_listing_invalid_values_when_passed_negative_numbers) {
+  string input = "1,-2,-4,5";
+  EXPECT_THROW(objUnderTest->Add(input), std::invalid_argument);
 }
