add73152a8fd96a7306e9386082c8abb64716a85
dhaneshECA <dhaneshutd@gmail.com>
2024-10-29 16:05:37 +0530

    Update SpeedMonitor.cpp

--- SpeedMonitor.cpp
+++ SpeedMonitor.cpp
@@ -7,14 +7,14 @@ void SpeedMonitor::monitor(IMessageCommunicator* communicator,ISpeedSensor* sens
             
             int currentSpeedInKmph=sensor->getCurrentSpeed();
             
-            logger->write("Current Speed In Kmph " + to_string(currentSpeedInKmph)+ "\n";
+            logger->write("Current Speed In Kmph " + to_string(currentSpeedInKmph)+ "\n");
             if(currentSpeedInKmph > _speedThreshold){
                   double mph = currentSpeedInKmph * 0.621371;
                   string message="Current Speed in Miles "+ to_string(mph);
                  int statusCode= communicator->pushMessage(message);
                   if(statusCode > 400){
                       //Log Message to Console
-                       logger->write("Error In Communication Unable to Contact Server " + message + "\n";
+                       logger->write("Error In Communication Unable to Contact Server " + message + "\n");
                   }
                  
             }
