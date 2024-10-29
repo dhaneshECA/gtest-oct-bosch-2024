3707d3aa857dae39dac656fcff41fceb4ba414bb
dhaneshECA <dhaneshutd@gmail.com>
2024-10-29 10:59:43 +0530

    Update StringCalculatorValueParameter.Tests.cpp

--- StringCalculatorValueParameter.Tests.cpp
+++ StringCalculatorValueParameter.Tests.cpp
@@ -1,1 +1,31 @@
+#include "StringCalculator.h"
+#include <gtest/gtest.h>
+#include <tuple>
 
+//Fixture class
+class StringCalculatorAddFixture:public testing::Test {
+  protected:
+    StringCalculator *objUnderTest;
+    void SetUp() override {
+      objUnderTest = new StringCalculator();
+    }
+    void TearDown() override {
+      delete objUnderTest;
+    }
+};
+
+//Value Parameterized Fixture class
+class StringCalculatorAddValueParamFixture: public StringCalculatorAddFixture, public testing::WithParamInterface<tuple<string,int>> {
+
+};
+
+INSTANTIATE_TEST_SUITE_P(ValidValuesDataSet,StringCalculatorAddValueParamFixture,testing::Values(
+make_tuple<"",0>, make_tuple<"0",0>, make_tuple<"1",1>, make_tuple<"1,2,3",6>
+));
+
+TEST_P(StringCalculatorAddValueParamFixture, DataDrivenTest) {
+  string input = std::get<0>(GetParam());
+  int expectedValue = std::get<1>(GetParam());
+  int actualValue = objUnderTest->Add(input);
+  EXPECT_EQ(expectedValue, actualValue);
+}
